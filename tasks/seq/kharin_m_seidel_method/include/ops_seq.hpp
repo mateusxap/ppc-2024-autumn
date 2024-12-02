ae5f7b599fe3ffd194d47e4fee125d3b6c597138
Kharin Matvey <harin.matveix@ya.ru>
2024-12-02 23:08:26 +0300

    Minifix clang format

--- tasks/seq/kharin_m_seidel_method/include/ops_seq.hpp
+++ tasks/seq/kharin_m_seidel_method/include/ops_seq.hpp
@@ -19,12 +19,12 @@ class GaussSeidelSequential : public ppc::core::Task {
   bool post_processing() override;
 
  private:
-  std::vector<double> a;  // Матрица коэффициентов в линейной форме
-  std::vector<double> b;  // Вектор свободных членов
-  std::vector<double> x;  // Вектор решений
-  std::vector<double> p;  // Предыдущее приближение
-  int n = 0;              // Размерность системы
-  double eps = 0.0;       // Точность вычислений
+  std::vector<double> a;       // Матрица коэффициентов в линейной форме
+  std::vector<double> b;       // Вектор свободных членов
+  std::vector<double> x;       // Вектор решений
+  std::vector<double> p;       // Предыдущее приближение
+  int n = 0;                   // Размерность системы
+  double eps = 0.0;            // Точность вычислений
   int max_iterations = 10000;  // Максимальное количество итераций
 };
 
